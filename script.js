9c2df8b4d12116fba3bed06052b72670a36e786e
Choszcz Jakub <jakub.choszcz@gmail.com>
2021-11-08 11:28:22 +0100

    Cwiczenia 1

---
+++ script.js
@@ -0,0 +1,71 @@
+// Ćwiczenie 1.1
+
+// const cw1_1 = (a, b, c) => {
+//     console.log('Enter an integer number: ' + a)
+//     console.log('Enter a real number: ' + b)
+//     console.log('Enter a character: ' + c)
+//     console.log(`${a}, ${b}, ${c}`)
+// }
+// cw1_1(1, 2.45, 's')
+
+
+
+
+
+// Ćwiczenie 1.2
+
+// const cw1_2 = (a, b, c) => {
+//     console.log(`Enter 3 integer numbers: ${a}, ${b}, ${c}`)
+
+//     const aritAver = (a + b + c) /2
+
+//     console.log('Arithmetic average = ' + aritAver)
+// }
+// cw1_2(2, 4 ,8)
+
+
+
+
+
+// Ćwiczenie 1.3
+
+// const cw1_3 = (a, b) => {
+//     const addition = a + b
+//     const multiplication = a * b
+//     const division = a / b
+//     const exponentiation = a**b
+//     const squareRoot = Math.sqrt(a)
+
+//     console.log(`Addition = ${addition}; Multiplication = ${multiplication}; Division = ${division}; Exponentiation = ${exponentiation}; Square Root = ${squareRoot}`)
+// }
+// cw1_3(2, 4)
+
+
+
+
+
+// Ćwiczenie 1.4
+
+// const cw1_4 = (a, b) => {
+//     let year = new Date().getFullYear()
+//     const age = year - b
+
+//     console.log(`${a}, masz ${age} lat.`)
+// }
+// cw1_4('Jasiu', '1989')
+
+
+
+
+
+// Ćwiczenie 1.5
+
+// const cw1_5 = (x, y, z) => {
+//     const p = Math.sqrt(x**2 + y**2 + z**2)
+//     const tan = y/x
+
+
+//     console.log(`${p}, ${tan}`)
+// }
+
+// cw1_5(-2, -1, 5)
