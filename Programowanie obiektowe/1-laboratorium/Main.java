afede7436da07c5601465832372b82249e74ea00
Jakub <jakub.choszcz@gmail.com>
2022-02-27 22:26:47 +0100

    Add oop lab 1

---
+++ Programowanie obiektowe/1-laboratorium/Main.java
@@ -0,0 +1,19 @@
+import java.lang.Math;
+
+public class Main {
+    public static void main(String args[]) {
+
+        int a = 1, b = 1, c = -2;
+        double delta = Math.pow(b, 2) - 4 * a * c;
+
+        double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+        double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+
+        System.out.println("x1: " + x1);
+        System.out.println("x2: " + x2);
+
+        for (double i = -3; i <= 3; i = (i * 10 + 1) / 10) {
+            System.out.println("Wartość funkcji dla x równego " + i + " = " + (a * Math.pow(i, 2) + b * i + c));
+        }
+    }
+}
